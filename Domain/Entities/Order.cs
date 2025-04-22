29562cda840b1ecf240ca0350e1c35cb1344df5c
CodesByUmar <asatillayevmuhammadumar00@gmail.com>
2025-04-22 12:57:23 +0500

    restaurant-management-system

---
+++ Domain/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int MenuId { get; set; }
+        public Menu Menu { get; set; } = null!;
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ Infrastructure/Data/RestaurantDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options) : base(options) { }
+
+
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
